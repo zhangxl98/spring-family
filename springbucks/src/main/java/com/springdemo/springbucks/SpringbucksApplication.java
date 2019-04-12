f02019f35aa1f4f330518e0b36d69c1aa9648bdb
zhangxl98 <651268723@qq.com>
2019-04-12 11:21:53 +0800

    测试生成订单

--- springbucks/src/main/java/com/springdemo/springbucks/SpringbucksApplication.java
+++ springbucks/src/main/java/com/springdemo/springbucks/SpringbucksApplication.java
@@ -1,13 +1,51 @@
 package com.springdemo.springbucks;
 
+import com.springdemo.springbucks.model.Coffee;
+import com.springdemo.springbucks.model.CoffeeOrder;
+import com.springdemo.springbucks.model.OrderState;
+import com.springdemo.springbucks.repository.CoffeeRepository;
+import com.springdemo.springbucks.service.CoffeeOrderService;
+import com.springdemo.springbucks.service.CoffeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Optional;
 
 @SpringBootApplication
-public class SpringbucksApplication {
+@Slf4j
+@EnableTransactionManagement
+@EnableJpaRepositories
+public class SpringbucksApplication implements ApplicationRunner {
+
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+    @Autowired
+    private CoffeeService coffeeService;
+    @Autowired
+    private CoffeeOrderService coffeeOrderService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbucksApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        // 查询所有的 Coffee 种类
+        log.info("All Coffee: {}",coffeeRepository.findAll());
+
+        // 按 name 查询 Coffee
+        Optional<Coffee> latte = coffeeService.findOneCoffee("Latte");
+        if (latte.isPresent()) {
+            CoffeeOrder order = coffeeOrderService.createOrder("Li Lei", latte.get());
+            log.info("Update INIT to PAID: {}", coffeeOrderService.updateState(order, OrderState.PAID));
+            log.info("Update PAID to INIT: {}", coffeeOrderService.updateState(order, OrderState.INIT));
+        }
+    }
 }
---
+++ springbucks/src/main/java/com/springdemo/springbucks/service/CoffeeService.java
@@ -0,0 +1,40 @@
+package com.springdemo.springbucks.service;
+
+import com.springdemo.springbucks.model.Coffee;
+import com.springdemo.springbucks.repository.CoffeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.exact;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author zhangxl98
+ * @Date 4/12/19 10:38 AM
+ * @OS Ubuntu 18.04 LTS
+ * @Device DELL-Inspiron-15-7559
+ * @Modified By
+ * @Version V1.0.0
+ * @Description
+ */
+@Service
+@Slf4j
+public class CoffeeService {
+
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
+    public Optional<Coffee> findOneCoffee(String name) {
+        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("name", exact().ignoreCase());
+        Optional<Coffee> coffee = coffeeRepository.findOne(Example.of(Coffee.builder().name(name).build(), matcher));
+
+        log.info("Coffee Find: {}", coffee);
+        return coffee;
+    }
+}
