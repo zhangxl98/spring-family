10b1a9b7d3467a4ccfe29b5c97a4c0bd71dde4e5
zhangxl98 <651268723@qq.com>
2019-04-13 10:36:14 +0800

    Copy springbucks TO Chapter03

---
+++ Chapter03/springbucks/src/main/java/com/springdemo/springbucks/model/Coffee.java
@@ -0,0 +1,34 @@
+package com.springdemo.springbucks.model;
+
+import lombok.*;
+import org.hibernate.annotations.Type;
+import org.joda.money.Money;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author zhangxl98
+ * @Date 4/12/19 10:28 AM
+ * @OS Ubuntu 18.04 LTS
+ * @Device DELL-Inspiron-15-7559
+ * @Modified By
+ * @Version V1.0.0
+ * @Description Coffee 实体类
+ */
+@Entity
+@Table(name = "T_COFFEE")
+@Builder
+@Data
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee extends BaseEntity implements Serializable {
+    private String name;
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyMinorAmount",
+            parameters = {@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
+    private Money price;
+}
