01cc0314415aa679f6565b1f2927f4c49fd5a7ed
zhangxl98 <651268723@qq.com>
2019-04-13 09:09:17 +0800

    MongoDB update 操作

--- Chapter04/mongo-demo/src/main/java/demo/spring/mongodemo/MongoDemoApplication.java
+++ Chapter04/mongo-demo/src/main/java/demo/spring/mongodemo/MongoDemoApplication.java
@@ -1,5 +1,6 @@
 package demo.spring.mongodemo;
 
+import com.mongodb.client.result.UpdateResult;
 import demo.spring.mongodemo.converter.MoneyReadConverter;
 import demo.spring.mongodemo.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import java.util.Arrays;
 import java.util.Currency;
@@ -36,13 +40,28 @@ public class MongoDemoApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Coffee latte = mongoTemplate.save(Coffee.builder().name("latte").price(Money.of(CurrencyUnit.of("CNY"), 30)).createTime(new Date()).updateTime(new Date())).build();
-        log.info("Latte: {}",latte);
+        Coffee latte = mongoTemplate.save(Coffee.builder().name("latte").price(Money.of(CurrencyUnit.of("CNY"), 30)).createTime(new Date()).updateTime(new Date()).build());
+        log.info("Latte: {}", latte);
         Thread.sleep(1000);
-        Coffee espresso = mongoTemplate.save(Coffee.builder().name("espresso").price(Money.of(CurrencyUnit.of("CNY"), 20)).createTime(new Date()).updateTime(new Date())).build();
-        log.info("Latte: {}",latte);
+        Coffee espresso = mongoTemplate.save(Coffee.builder().name("espresso").price(Money.of(CurrencyUnit.of("CNY"), 20)).createTime(new Date()).updateTime(new Date()).build());
+        log.info("Latte: {}", espresso);
+
+        List<Coffee> coffeeList = mongoTemplate.find(Query.query(Criteria.where("name").is("latte")), Coffee.class);
+        log.info("latteSize: {}", coffeeList.size());
+        coffeeList.forEach(c -> log.info("latte: {}", c));
+
+        Thread.sleep(1000);
+        // 更新文档
+        UpdateResult result = mongoTemplate.updateFirst(Query.query(Criteria.where("name").is("latte")), new Update().set("price", Money.of(CurrencyUnit.of("CNY"), 50)).currentDate("updateTime"), Coffee.class);
+        log.info("UpdateCoffeeCount: {}", result.getModifiedCount());
+        log.info("UpdateCoffeeItem: {}", mongoTemplate.findById(latte.getId(), Coffee.class));
+
+
         List<Coffee> coffees = mongoTemplate.findAll(Coffee.class);
-        coffees.forEach(c -> log.info("Coffee: {}", c));
+        log.info("coffeeSize: {}", coffees.size());
+        coffees.forEach(c -> log.info("AllCoffee: {}", c));
+
+        // 删除所有文档
         coffees.forEach(c -> mongoTemplate.remove(c));
     }
 }
