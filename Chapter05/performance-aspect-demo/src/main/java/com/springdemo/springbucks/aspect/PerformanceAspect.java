07134835bf3f2dd06d276e6e83ee37cd5629bcfb
zhangxl98 <651268723@qq.com>
2019-04-19 15:35:29 +0800

    使用 AOP 打印数据访问层摘要

---
+++ Chapter05/performance-aspect-demo/src/main/java/com/springdemo/springbucks/aspect/PerformanceAspect.java
@@ -0,0 +1,45 @@
+package com.springdemo.springbucks.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author zhangxl98
+ * @Date 4/19/19 11:28 AM
+ * @OS Ubuntu 18.04 LTS
+ * @Device DELL-Inspiron-15-7559
+ * @Modified By
+ * @Version V1.0.0
+ * @Description
+ */
+@Aspect
+@Component
+@Slf4j
+public class PerformanceAspect {
+    @Around("repositoryOps()")
+    public Object logPerformance(ProceedingJoinPoint pjp) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        String name = "-";
+        String result = "Y";
+        try {
+            name = pjp.getSignature().toShortString();
+            return pjp.proceed();
+        } catch (Throwable t) {
+            result = "N";
+            throw t;
+        } finally {
+            long endTime = System.currentTimeMillis();
+            log.info("{};{};{}ms", name, result, endTime - startTime);
+        }
+    }
+
+    @Pointcut("execution(* com.springdemo.springbucks.repository..*(..))")
+    private void repositoryOps() {
+    }
+}
